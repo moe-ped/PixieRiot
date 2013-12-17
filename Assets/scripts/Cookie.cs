cd9a3275b1fcbd4afe3906747ed5bf4c7fccdc61
moe-ped <m.berning@web.de>
2013-12-17 21:52:23 +0100

    Fix (I hope)

---
+++ Assets/scripts/Cookie.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cookie : MonoBehaviour {
+
+	public AudioClip hitSound;
+	public AudioClip crackSound;
+
+	public Transform dust;
+
+	public int hp = 3;
+
+	void OnCollisionEnter2D (Collision2D other) {
+		Main main = Camera.main.GetComponent("Main") as Main;
+		if (other.relativeVelocity.magnitude > 10) {
+			audio.PlayOneShot(crackSound);
+			if (other.gameObject.GetComponent ("Arrow")) {
+				Arrow arrow = other.gameObject.GetComponent ("Arrow") as Arrow;
+				main.teams[main.turn].score++;
+				hp -= arrow.dp;
+			}
+			else {
+				hp -= 1;
+			}
+			if (hp <= 0) {
+				Instantiate(dust, transform.position, Quaternion.identity);
+				float initVolume = main.audio.volume;
+				main.audio.volume = 0.5f;
+				main.audio.PlayOneShot(main.cookieDieSound);
+				main.audio.volume = initVolume;
+				Destroy(gameObject);
+			}
+		}
+		else if (other.relativeVelocity.magnitude > 2) {
+			audio.PlayOneShot(hitSound);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//placeholder
+		renderer.material.color = new Color(1, (float)hp/3, (float)hp/3);
+	}
+}
---
+++ Assets/scripts/buttons/ScoreBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreBar : MonoBehaviour {
+
+	float initialScaley = 1;
+
+	public int side;
+
+	public float height;
+
+	float initialPositiony;
+
+	// Use this for initialization
+	void Start () {
+		initialScaley = transform.localScale.y;
+		initialPositiony = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Main main = Camera.main.GetComponent("Main") as Main;
+		float scorePercent = main.teams[side].score/main.maxScore;
+		float newScaley = initialScaley * scorePercent;
+		transform.position = new Vector3 (transform.position.x, initialPositiony - (height/2 * (1-scorePercent)), transform.position.z); 
+		transform.localScale = new Vector3 (transform.localScale.x, newScaley, transform.localScale.z);
+	}
+}
