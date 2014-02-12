baac72e131bdeebff65be538130dc1ac792cc4cc
moe-ped <m.berning@web.de>
2014-02-12 19:57:47 +0100

    addedfilesonceagain

---
+++ Assets/scripts/Dust.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dust : MonoBehaviour {
+
+	public float lifetime;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
