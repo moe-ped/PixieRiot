4e12969c450a897ed318d8bcfa016af1f8bca833
moe-ped <m.berning@web.de>
2014-02-23 17:43:34 +0100

    obligatory fix

---
+++ Assets/scripts/buttons/WeaponSelect.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSelect : MonoBehaviour {
+
+	public int team;
+	public Transform[] buttons;
+	Main main;
+
+	// Use this for initialization
+	void Start () {
+		main = Camera.main.GetComponent("Main") as Main;
+		texturizeButtons ();
+		colorizeButtons ();
+	}
+
+	void texturizeButtons () {
+		for (int b=0; b<buttons.Length; b++) {
+			buttons[b].gameObject.renderer.material.mainTexture = main.weapons[b].icon;
+		}
+	}
+
+	public void colorizeButtons () {
+		for (int b=0; b<buttons.Length; b++) {
+			WeaponSlot weaponslot = buttons[b].GetComponent("WeaponSlot") as WeaponSlot;
+			if (main.teams[team].selectedWeapon == b) {
+				buttons[b].gameObject.renderer.material.color = Color.white;
+				weaponslot.normal = Color.white;
+			}
+			else if (!main.weapons[b].available) {
+				weaponslot.available = false;
+				buttons[b].gameObject.renderer.material.color = new Color (0.7f, 0.3f, 0.3f);
+				weaponslot.normal = new Color (0.7f, 0.3f, 0.3f);
+			}
+			else {
+				weaponslot.available = true;
+				buttons[b].gameObject.renderer.material.color = Color.grey;
+				weaponslot.normal = Color.grey;
+			}
+		}
+	}
+
+	public void selectWeapon (int index) {
+		main.teams[team].selectedWeapon = index;
+	}
+
+	public void thwart (bool greenLight) {
+		main.MouseOver = greenLight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (main.turnChange) {
+			colorizeButtons ();
+		}
+	}
+}
---
+++ Assets/scripts/buttons/WeaponSlot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSlot : MonoBehaviour {
+
+	WeaponSelect weaponSelect;
+
+	public Color normal = new Color (0.7f, 0.3f, 0.3f);
+
+	public bool available = true;
+
+	void Start () {
+		weaponSelect = transform.parent.GetComponent ("WeaponSelect") as WeaponSelect;
+	}
+
+	void OnMouseDown () {
+		for (int i=0; i<weaponSelect.buttons.Length; i++) {
+			if (weaponSelect.buttons[i] == this.transform && available) {
+				weaponSelect.selectWeapon(i);
+				weaponSelect.colorizeButtons ();
+			}
+		}
+	}
+
+	//disable throwing in main
+	void OnMouseEnter () {
+		weaponSelect.thwart (true);
+		if (available) {
+			renderer.material.color = Color.green;
+		}
+	}
+
+	void OnMouseExit () {
+		weaponSelect.thwart (false);
+		if (available) {
+			renderer.material.color = normal;
+		}
+	}
+
+	void Update () {
+
+	}
+}
